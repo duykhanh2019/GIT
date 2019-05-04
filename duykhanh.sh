d526f249ed1066d4b595a922fd40562099d3ba62
Duy Khanh <khanhpd.cenland@gmail.com>
2019-05-04 10:46:05 +0700

    Add file shellscript

---
+++ duykhanh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd Shellscript;pwd
+ls -d * | while read dir; do
+       rm -rf "$dir"
+done
+cp /home/duykhanh/duykhanh.sh duykhanh.sh;cp /home/duykhanh/idol.txt idol.txt;
+
+s=`date +%u`
+r=$((RANDOM%20+1))
+for (( i = 1; i <=7; i++))
+do
+        a=`expr $i - $s`
+        x=`date -d "$a"day +"%d_%m_%Y"`
+        GetKeyWord=`sed -n $i'p' idol.txt`
+        GetImageLink=$(wget --user-agent 'Mozilla/5.0' -qO - "https://www.google.com/search?q=$GetKeyWord\&tbm=isch" | sed 's/</\n</g' | grep '<img' | tail -"$r" | head -1 | sed 's/.*src="\([^"]*\)".*/\1/')
+#Tao tep moi 
+mkdir $x
+#tai anh vao thu muc
+wget $GetImageLink -O $x/$GetKeyWord.jpg
+done
+
